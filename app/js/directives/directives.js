f8ef58592508270b0ee715715937f592f4b07fdd
Fateh Khalsa <fskhalsa@gmail.com>
2013-09-07 14:17:27 -0600

    Main layout

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -8,3 +8,25 @@ angular.module('queueapp').
       elm.text(version);
     };
   }]);
+
+
+
+angular.module('queueapp')
+.directive('listItem', function () {
+	return {
+		restrict: 'E',
+		template: '<div class="listItem">\
+			        	<img src="http://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Gold-on-the-ceiling.jpg/220px-Gold-on-the-ceiling.jpg" />\
+			    		<table class="info">\
+			        		<tr class="title"><td>Gold on the Ceiling</td></tr>\
+			        		<tr class="subTitle"><td>The Black Keys</td></tr>\
+			    		</table>\
+			    	</div>',
+		replace: true,
+		link: function (scope, elem, attrs) {
+			elem.on('click', function (event) {
+				elem.addClass('selected');
+			})
+		}
+	}
+})
