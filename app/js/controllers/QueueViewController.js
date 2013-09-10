2d3e930ff51d45de25cdddecb8321d8d57eca75c
Fateh Khalsa <fskhalsa@gmail.com>
2013-09-09 23:36:05 -0600

    Detail view display

--- app/js/controllers/QueueViewController.js
+++ app/js/controllers/QueueViewController.js
@@ -8,12 +8,14 @@ controller('QueueViewCtrl', ['$scope', '$rootScope', 'Queue',
 
         $scope.changeCurrentItem = function(index) {
             $scope.currentItemIndex = index;
+            $scope.currentItem = $scope.queue[index];
         }
 
         $rootScope.$on('login', function() {
             Queue.getItems().then(function(items) {
                 $scope.queue = items;
                 $scope.currentItemIndex = 0;
+                $scope.currentItem = $scope.queue[$scope.currentItemIndex];
             })
         })
 
