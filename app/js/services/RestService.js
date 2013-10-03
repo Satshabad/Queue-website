3b3f5149d1251acc3bc9853a4011165e638d9e00
Satshabad Khalsa <satshabad.music@gmail.com>
2013-10-02 19:19:56 -0700

    added more services

---
+++ app/js/services/RestService.js
@@ -0,0 +1,117 @@
+'use strict';
+
+angular.module('queueapp').
+factory('Queue', function($resource, User) {
+
+    var Queue = $resource('http://localhost:8000/user/:userId/sent/:id', {
+        id: '@id',
+        userId: _findUserId
+    }, {
+        query: {
+            isArray: true,
+            method: 'get',
+            transformResponse: function(data, headers) {
+                return JSON.parse(data).queue.items;
+            }
+        }
+    });
+
+    function getItems() {
+        return Queue.query().$promise
+    }
+
+    function _findUserId() {
+        var id = User.getUser().id;
+
+        if (id === undefined) {
+            throw "User not authenticated"
+        }
+
+        return id;
+    }
+
+
+    return {
+        "getItems" :  getItems
+    }
+
+
+})
+
+
+angular.module('queueapp').
+factory('Saved', function($resource, User) {
+
+    var Saved = $resource('http://localhost:8000/user/:userId/sent/:id', {
+        id: '@id',
+        userId: _findUserId
+    }, {
+        query: {
+            isArray: true,
+            method: 'get',
+            transformResponse: function(data, headers) {
+                return JSON.parse(data).Saved.items;
+            }
+        }
+    });
+
+    function getItems() {
+        return Saved.query().$promise
+    }
+
+    function _findUserId() {
+        var id = User.getUser().id;
+
+        if (id === undefined) {
+            throw "User not authenticated"
+        }
+
+        return id;
+    }
+
+
+    return {
+        "getItems" :  getItems
+    }
+
+
+})
+
+
+angular.module('queueapp').
+factory('Sent', function($resource, User) {
+
+    var Sent = $resource('http://localhost:8000/user/:userId/sent/:id', {
+        id: '@id',
+        userId: _findUserId
+    }, {
+        query: {
+            isArray: true,
+            method: 'get',
+            transformResponse: function(data, headers) {
+                return JSON.parse(data).Sent.items;
+            }
+        }
+    });
+
+    function getItems() {
+        return Sent.query().$promise
+    }
+
+    function _findUserId() {
+        var id = User.getUser().id;
+
+        if (id === undefined) {
+            throw "User not authenticated"
+        }
+
+        return id;
+    }
+
+
+    return {
+        "getItems" :  getItems
+    }
+
+
+})
