9c684502b52135ff2a5dd93dcb91f1a197dd89a2
Satshabad Khalsa <satshabad.music@gmail.com>
2013-10-05 18:54:52 -0700

    picker work

--- app/js/controllers/AddViewControllers.js
+++ app/js/controllers/AddViewControllers.js
@@ -1,23 +1,51 @@
 angular.module('queueapp').
-controller('ShareViewCtrl', ['$scope', 'Search', 'Listens', 'Facebook',
+controller('ModalAddViewCtrl', ['$scope',
+    function($scope) {
+
+    }
+])
+
+angular.module('queueapp').
+controller('PickerViewCtrl', ['$scope', 'Search', 'Listens', 'Facebook',
     function($scope, Search, Listens, Facebook) {
 
+        $scope.mode = 'listens' // or 'search'
+
         $scope.listens = [];
-        $scope.searchResults= [];
+        $scope.cachedListens = [];
+
+        $scope.searchResults = [];
+        $scope.cachedSearchResults = [];
         $scope.searchResults.busy = false;
 
-        $scope.changeSelectedItem = function(index) {
+        $scope.enableSearchMode = function() {
+            $scope.mode = 'search'
+            $scope.listens = [];
+            $scope.searchResults = $scope.cachedSearchResults;
+        };
+
+        $scope.enableListensMode = function() {
+            $scope.mode = 'listens'
+            $scope.searchResults = [];
+            $scope.listens = $scope.cachedListens;
+        };
+
+        $scope.changeSelectedItem = function(index, item) {
             $scope.selectedItemIndex = index;
-            $scope.currentItem = $scope.queue[index];
+
+            $scope.currentItem = item
+            $scope.$parent.selectedItem = item
+
         }
 
         Listens.getTracks().then(function(tracks) {
             $scope.listens = tracks;
+            $scope.cachedListens = tracks;
         })
 
-        $scope.doSearch = function () {
+        $scope.doSearch = function() {
 
-            if ($scope.searchResults.busy === true){
+            if ($scope.searchResults.busy === true) {
                 return
             }
 
@@ -26,11 +54,19 @@ controller('ShareViewCtrl', ['$scope', 'Search', 'Listens', 'Facebook',
 
             Search.getTracks(searchTerm).then(function(results) {
                 $scope.searchResults = results;
+                $scope.cachedSearchResults = results
                 $scope.searchResults.busy = false;
             })
 
         };
 
+    }
+])
 
-}])
 
+angular.module('queueapp').
+controller('ShareViewCtrl', ['$scope', 'Facebook',
+    function($scope, Facebook) {
+
+    }
+])
