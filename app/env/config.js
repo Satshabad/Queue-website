bb48522baa11d356e319a0213535ede56a4994a4
Satshabad Khalsa <satshabad.music@gmail.com>
2013-11-01 22:37:02 -0700

    added new app id

--- app/env/config.js
+++ app/env/config.js
@@ -8,6 +8,6 @@ if (window.location.hostname == "queuemusic.me"){
 
   angular.module('queueapp').constant('config', {
         "hostname": "localhost:8000",
-        "appId":   '580401268636769'
+        "appId":   '1380227372202033'
     });
 }
